0a264feea4793d2e4a4e870f08730de43f5cb00e
b3n10 <francisben.lleve@gmail.com>
2018-01-03 10:13:40 +0800

    Done all 8 exercises

--- day4/app.js
+++ day4/app.js
@@ -1,5 +1,3 @@
-//current time in video - https://youtu.be/HB1ZC7czKRs?list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH&t=618 (10:18)
-
 const inventors = [
 	{ first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
 	{ first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -14,8 +12,6 @@ const inventors = [
 	{ first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
 	{ first: 'Hanna', last: 'HammarstrÃ¶m', year: 1829, passed: 1909 }
 ];
-
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 //get people who were born in 1500s using filter
 const fifteen = inventors.filter( inventor => inventor.year >= 1500 && inventor.year < 1600 );
@@ -35,4 +31,33 @@ const totalYears = inventors.reduce( (total, inventor) => {
 }, 0);
 //console.log(totalYears);
 
+//sort inventors by years lived
+const oldest = inventors.sort( (a, b) => ((a.passed - a.year) > (b.passed - b.year)) ? -1 : 1);
+//console.log(oldest);
+
+//create list of Boulevards in Paris that contain 'de' anywhere in the name
+//from this page: https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+//answer:
+//const boulevards = Array.from(document.querySelectorAll('.mw-category li'));
+//boulevards.map( c => c.innerText ).filter( d => d.includes('de') );
+
+//sort people alphabetically by last name
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+const alpha = people.sort( (currentName, nextName) => {
+	const [lastNameA, firstNameA] = currentName.split(", ");
+	const [lastNameB, firstNameB] = nextName.split(", ");
+	return (lastNameA > lastNameB) ? 1 : -1;
+});
+//console.log(alpha);
+
+//Sum up instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+const transpo = data.reduce( (obj, item) => {
+	if (!obj[item]) { obj[item] = 0 }
+	obj[item]++;
+	return obj;
+}, {});
+
+console.log(transpo);
