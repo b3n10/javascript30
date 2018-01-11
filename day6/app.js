5c550611bdab0c6d9e4eaadb4413d9cf4126cba1
b3n10 <francisben.lleve@gmail.com>
2018-01-11 16:28:44 +0800

    Done: # of search results

--- day6/app.js
+++ day6/app.js
@@ -13,21 +13,32 @@ function findPlace(searchPlace, cities) {
 }
 
 function displayMatches() {
-	const matchPlace = findPlace(this.value, cities);
-	const li = matchPlace.map(place => {
-		const regex = new RegExp(this.value, 'gi');
-		const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-		const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-		return `
-			<li>
+	let counter = 0; 
+	result.innerHTML = "0";
+
+	if (this.value) {
+		const matchPlace = findPlace(this.value, cities);
+		const li = matchPlace.map(place => {
+			counter++;
+			result.innerHTML = counter;
+
+			const regex = new RegExp(this.value, 'gi');
+			const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+			const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+			return `
+				<li>
 				<span>${cityName}, ${stateName}</span>
 				<span>${place.population}</span>
-			</li>
+				</li>
+				`;
+		}).join('');//without join lists will show comma in between them
+		suggestions.innerHTML = li;//add it to ul
+	} else {
+		suggestions.innerHTML = `
+				<li>Filter by city</li>
+				<li>or a state</li>
 			`;
-	}).join('');//without join lists will show comma in between them
-
-
-	suggestions.innerHTML = li;
+	}
 }
 
 const searchInput = document.querySelector(".search");
