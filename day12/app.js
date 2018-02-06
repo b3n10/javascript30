71c38d782bc4e2b9900b518d3d5edc3fee0b96a4
b3n10 <francisben.lleve@gmail.com>
2018-02-06 18:03:32 +0800

    Done day12

---
+++ day12/app.js
@@ -0,0 +1,14 @@
+const pressed = [];
+const secretCode = "hek0424!hek";
+
+window.addEventListener("keyup", (e) => {
+	if (e.key == "Alt" || e.key == "Shift" || e.key == "Meta" || e.key == "Control" || e.key == "Tab" || e.key == " ") {
+		return;
+	} else {
+		pressed.push(e.key);
+		pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+		if (pressed.join("").includes(secretCode)) {
+			console.log("WiFi password is " + pressed.join(""));
+		}
+	}
+});
