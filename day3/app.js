1cc8736b7d02bd9de4ae3d69d85f7990ef3b048f
b3n10 <francisben.lleve@gmail.com>
2018-02-10 06:41:30 +0800

    Move app.js to src/main.js

--- day3/app.js
+++
@@ -1,9 +0,0 @@
-const inputs = document.querySelectorAll("input");
-
-function imgUpdate() {
-	const suffix = this.dataset.sizing || '';
-	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-}
-
-inputs.forEach(input => input.addEventListener("change", imgUpdate));
-inputs.forEach(input => input.addEventListener("mousemove", imgUpdate));
