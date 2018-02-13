0ba82eb89b8574f2ab0d3b36fd153cc6cb9daca1
b3n10 <francisben.lleve@gmail.com>
2018-02-14 05:05:02 +0800

    Update day 14

--- day14/lib/src/main.js
+++ day14/lib/src/main.js
@@ -1,6 +1,8 @@
+"use strict";
+
 // copy
-let dog = "budoy";
-let puppy = dog;
+var dog = "budoy";
+var puppy = dog;
 console.log(dog, puppy);
 
 dog = "newboy";
@@ -8,43 +10,43 @@ console.log(dog, puppy);
 console.log();
 
 // reference
-const lakers = ["clarkson", "ball", "walton"];
+var lakers = ["clarkson", "ball", "walton"];
 
 // use slice to make a copy and original is untouched, no args to copy all values
 // method1:
-const lakers2 = lakers.slice();
+var lakers2 = lakers.slice();
 lakers2[0] = "clarkson";
 console.log("Lakers: ", lakers);
 console.log("Slice Lakers: ", lakers2);
 
 // or make a new array and use concat with the array to copy as args
 // method2:
-const lakers3 = [].concat(lakers);
+var lakers3 = [].concat(lakers);
 lakers3[0] = "clarkson";
 console.log("Lakers: ", lakers);
 console.log("Concat Lakers: ", lakers3);
 
 // method3:
-const lakers4 = Array.from(lakers);
+var lakers4 = Array.from(lakers);
 lakers4[2] = "kobe";
 console.log("Lakers: ", lakers);
 console.log("Array from Lakers: ", lakers4);
 
 // using spread
 // method4:
-const lakers5 = [...lakers];
+var lakers5 = [].concat(lakers);
 lakers5[2] = "payton";
 console.log("Lakers: ", lakers);
 console.log("Spread Lakers: ", lakers5);
 
 // copy and make changes on original array
-const lakers1 = lakers;
+var lakers1 = lakers;
 lakers1[0] = "thomas";
 console.log("Copy Lakers: ", lakers1);
 console.log("Lakers: ", lakers);
 
 // cloning objects
-const warriors = {
+var warriors = {
 	coach: "Steve Kerr",
 	manager: "Dan Gilbert",
 	roster: {
@@ -53,7 +55,7 @@ const warriors = {
 	}
 };
 
-const war2 = Object.assign({}, warriors);
+var war2 = Object.assign({}, warriors);
 war2.manager = "Popovich";
 console.log(warriors);
 console.log(war2);
